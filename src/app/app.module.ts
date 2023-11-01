2f3357e70e01945ce59e26c130ebab20dce63878
Cedy <58510598+cedylad@users.noreply.github.com>
2023-11-01 15:24:35 +0100

    Header ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProjetComponent } from './projet/projet.component';
 import { ProjetListComponent } from './projet-list/projet-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProjetComponent,
     ProjetListComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
