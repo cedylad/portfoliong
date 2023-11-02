7f3df53ce30abce89cca8088f8f6488e22f14813
Cedy <58510598+cedylad@users.noreply.github.com>
2023-11-02 12:20:04 +0100

    Style : Add Resp All

--- src/app/services/projets.services.ts
+++ src/app/services/projets.services.ts
@@ -1,65 +1,45 @@
-import { Injectable } from "@angular/core";
-import { Projet } from "../models/projet.model";
+import { Injectable } from '@angular/core';
+import { Projet } from '../models/projet.model';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
+export class ProjetsServices {
+  projets: Projet[] = [
+    {
+      id: 1,
+      titleProjet: 'HuberEat',
+      imgProjet: '/assets/projets/hubereat.mov',
+      descriptionProjet:
+        "HubertEat est un site web construit en utilisant PHP selon le modèle MVC. Il s'agit d'une plateforme de commande de nourriture. Ce projet a été développé dans le cadre d'un examen.",
+      githubProjet: 'https://github.com/cedylad/HubertEat',
+    },
+    {
+      id: 2,
+      titleProjet: 'Projet 2',
+      imgProjet:
+        'https://images.unsplash.com/photo-1474511320723-9a56873867b5?auto=format&fit=crop&q=80&w=2072&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      descriptionProjet: 'Desription projet 2',
+    },
+    {
+      id: 3,
+      titleProjet: 'Projet 3',
+      imgProjet:
+        'https://images.unsplash.com/photo-1474511320723-9a56873867b5?auto=format&fit=crop&q=80&w=2072&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      descriptionProjet: 'Desription projet 3',
+    },
+  ];
 
-export class ProjetsServices{
-    projets: Projet[] = [
-        {
-            id: 1,
-            titleProjet: 'HuberEat',
-            imgProjet: '/assets/projets/hubereat.mov',
-            descriptionProjet: "HubertEat est un site web construit en utilisant PHP selon le modèle MVC. Il s'agit d'une plateforme de commande de nourriture. Ce projet a été développé dans le cadre d'un examen.",
-            stateProjet: 'Terminé',
-            frameworkProjet: 'Boostrap',
-            lanagageProjet: 'PHP',
-            githubProjet: 'https://github.com/cedylad/HubertEat',
-        },
-        {
-            id: 2,
-            titleProjet: 'Projet 2',
-            imgProjet: 'https://images.unsplash.com/photo-1474511320723-9a56873867b5?auto=format&fit=crop&q=80&w=2072&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            descriptionProjet: 'Desription projet 2',
-        },
-        {
-            id: 3,
-            titleProjet: 'Projet 3',
-            imgProjet: 'https://images.unsplash.com/photo-1474511320723-9a56873867b5?auto=format&fit=crop&q=80&w=2072&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            descriptionProjet: 'Desription projet 3',
-        },
-        {
-            id: 4,
-            titleProjet: 'Projet 1',
-            imgProjet: 'https://images.unsplash.com/photo-1474511320723-9a56873867b5?auto=format&fit=crop&q=80&w=2072&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            descriptionProjet: 'Desription projet 1',
-            stateProjet: 'En attente',
-        },
-        {
-            id: 5,
-            titleProjet: 'Projet 2',
-            imgProjet: 'https://images.unsplash.com/photo-1474511320723-9a56873867b5?auto=format&fit=crop&q=80&w=2072&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            descriptionProjet: 'Desription projet 2',
-        },
-        {
-            id: 6,
-            titleProjet: 'Projet 3',
-            imgProjet: 'https://images.unsplash.com/photo-1474511320723-9a56873867b5?auto=format&fit=crop&q=80&w=2072&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-            descriptionProjet: 'Desription projet 3',
-        },
-      ];
+  getAllProjets(): Projet[] {
+    return this.projets;
+  }
 
-      getAllProjets(): Projet[]{
-        return this.projets;
-      }
-
-      getProjetById(projetId: number): Projet {
-        const projet =  this.projets.find(projet => projet.id === projetId);
-        if (!projet) {
-            throw new Error('Projet not found!');
-        } else {
-            return projet;
-        }
-      }
+  getProjetById(projetId: number): Projet {
+    const projet = this.projets.find((projet) => projet.id === projetId);
+    if (!projet) {
+      throw new Error('Projet not found!');
+    } else {
+      return projet;
+    }
+  }
 }
