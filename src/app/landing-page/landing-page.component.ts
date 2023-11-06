a444144c1faea4a93e0804b1acc258d6a866889d
Cedy <58510598+cedylad@users.noreply.github.com>
2023-11-06 22:06:11 +0100

    Animation sur le call to aciton

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-landing-page',
@@ -9,7 +10,21 @@ import { Router } from '@angular/router';
 export class LandingPageComponent implements OnInit {
   constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    //Bouton décovurir "mes projets"
+    gsap.from('.btn-discover', {
+      duration: 1,
+      y: 30,
+      repeat: -1,
+      yoyo: true,
+    });
+
+    //Bouton "hi"
+    gsap.from('.hi', {
+      duration: 2,
+      x: 500,
+    });
+  }
 
   onContinue(): void {
     this.router.navigateByUrl('projets');
